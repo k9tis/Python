cc08dec2d9e40693f74bf3379917e072b7b07fd9
k9tis <bakin-maxim@bk.ru>
2019-03-17 13:00:28 +0700

    first commit

---
+++ 20.py
@@ -0,0 +1,9 @@
+chetnie=0
+
+a =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+for  i in  a:
+    if i%2==0:
+        chetnie += 1
+
+
+print(chetnie)
---
+++ 51.py
@@ -0,0 +1,9 @@
+a=[1,2,3,4]
+for i in range(a):
+ if i<2 : 
+    print(i)
+for i2 in range(a):
+ if i2<3 : 
+    print(i2)
+
+
